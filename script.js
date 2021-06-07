612e772b13449a8fd99a3c2f82aa4f44dfda541f
PandjiBayu <pandjibayusatria@gmail.com>
2021-06-07 21:05:48 +0800

    Final Project

---
+++ script.js
@@ -0,0 +1,125 @@
+function DarkMode() {
+// body
+var element = document.body;
+element.classList.toggle("body-dark");
+
+// header
+var darkhead = document.getElementById("header").className === "header";
+    if (darkhead) {
+        document.getElementById("header").className = "header-dark";
+    } else if (!darkhead) {
+        document.getElementById("header").className = "header";
+    }
+
+var darkhead = document.getElementById("header-r").className === "header-r";
+    if (darkhead) {
+        document.getElementById("header-r").className = "header-r-dark";
+    } else if (!darkhead) {
+        document.getElementById("header-r").className = "header-r";
+    }
+
+var darkhead = document.getElementById("header-l").className === "header-l";
+    if (darkhead) {
+        document.getElementById("header-l").className = "header-l-dark";
+    } else if (!darkhead) {
+        document.getElementById("header-l").className = "header-l";
+    }
+
+var darkhead = document.getElementById("menu-bar").className === "menu-bar";
+    if (darkhead) {
+        document.getElementById("menu-bar").className = "menu-bar-dark";
+    } else if (!darkhead) {
+        document.getElementById("menu-bar").className = "menu-bar";
+    }
+
+// home
+var darkhome = document.getElementById("home").className === "home";
+    if (darkhome) {
+        document.getElementById("home").className = "home-dark";
+    } else if (!darkhome) {
+        document.getElementById("home").className = "home";
+    }
+
+var darkhome = document.getElementById("origin").className === "origin";
+    if (darkhome) {
+        document.getElementById("origin").className = "origin-dark";
+    } else if (!darkhome) {
+        document.getElementById("origin").className = "origin";
+    }
+
+// about
+var darkabout = document.getElementById("about-content").className === "about-content";
+    if (darkabout) {
+        document.getElementById("about-content").className = "about-content-dark";
+    } else if (!darkabout) {
+        document.getElementById("about-content").className = "about-content";
+    }
+
+var darkabout = document.getElementById("aboutMe").className === "aboutMe";
+    if (darkabout) {
+        document.getElementById("aboutMe").className = "aboutMe-dark";
+    } else if (!darkabout) {
+        document.getElementById("aboutMe").className = "aboutMe";
+    }
+
+// experience
+var darkexperience = document.getElementById("experience").className === "experience";
+    if (darkexperience) {
+        document.getElementById("experience").className = "experience-dark";
+    } else if (!darkexperience) {
+        document.getElementById("experience").className = "experience";
+    }
+
+var darkexperience = document.getElementById("study-content").className === "study-content";
+    if (darkexperience) {
+        document.getElementById("study-content").className = "study-content-dark";
+    } else if (!darkexperience) {
+        document.getElementById("study-content").className = "study-content";
+    }
+
+var darkexperience = document.getElementById("work-content").className === "work-content";
+    if (darkexperience) {
+        document.getElementById("work-content").className = "work-content-dark";
+    } else if (!darkexperience) {
+        document.getElementById("work-content").className = "work-content";
+    }
+
+// business
+var darkbusiness = document.getElementById("business").className === "business";
+    if (darkbusiness) {
+        document.getElementById("business").className = "business-dark";
+    } else if (!darkbusiness) {
+        document.getElementById("business").className = "business";
+    }
+
+// contact
+var darkcontact = document.getElementById("contact").className === "contact";
+    if (darkcontact) {
+        document.getElementById("contact").className = "contact-dark";
+    } else if (!darkcontact) {
+        document.getElementById("contact").className = "contact";
+    }
+
+var darkcontact = document.getElementById("contact-form").className === "contact-form";
+    if (darkcontact) {
+        document.getElementById("contact-form").className = "contact-form-dark";
+    } else if (!darkcontact) {
+        document.getElementById("contact-form").className = "contact-form";
+    }
+
+// footer
+var darkfooter = document.getElementById("footer").className === "footer";
+    if (darkcontact) {
+        document.getElementById("footer").className = "footer-dark";
+    } else if (!darkcontact) {
+        document.getElementById("footer").className = "footer";
+    }
+
+var darkfooter = document.getElementById("footer-h3").className === "footer-h3";
+    if (darkcontact) {
+        document.getElementById("footer-h3").className = "footer-h3-dark";
+    } else if (!darkcontact) {
+        document.getElementById("footer-h3").className = "footer-h3";
+    }
+
+}
